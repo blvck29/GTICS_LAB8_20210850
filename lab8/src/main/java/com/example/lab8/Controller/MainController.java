41ea99c3b280d2bdc2eca6280e266d0ef0595c7f
blvck29 <a20210850@pucp.edu.pe>
2024-06-12 21:04:00 -0500

    aaa

--- lab8/src/main/java/com/example/lab8/Controller/MainController.java
+++ lab8/src/main/java/com/example/lab8/Controller/MainController.java
@@ -13,17 +13,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@Controller(value = "/api/pokeapi")
+@Controller
+@RequestMapping("/pokeapi")
 public class MainController {
-
 
     final PokemonRepo pokemonRepo;
     final UserRepo userRepo;
-
     final UserPokemonRepo userPokemonRepo;
 
     @Autowired
